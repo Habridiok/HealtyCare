32052fd1a5ce62545255706bb5710d9d10cae655
DESKTOP-SKAJIBU\Global Store <habridio88@gmail.com>
2017-09-10 21:24:36 +0700

    loginfail fix bug

--- HealthyCare/app/src/main/java/com/example/adewijanugraha/healthycare/LoginFail.java
+++ HealthyCare/app/src/main/java/com/example/adewijanugraha/healthycare/LoginFail.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.io;
 
 public class LoginFail extends AppCompatActivity {
 
